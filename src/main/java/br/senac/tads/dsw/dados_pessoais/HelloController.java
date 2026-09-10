8f4071b40d872cbc5c5f7e0070415a946b487c6e
Rassati <feliperassati@gmail.com>
2026-09-10 13:16:26 -0300

    Terminada a ETAPA 00.

--- src/main/java/br/senac/tads/dsw/dados_pessoais/HelloController.java
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/HelloController.java
@@ -1,7 +1,10 @@
 package br.senac.tads.dsw.dados_pessoais;
 
+import tools.jackson.core.JacksonException;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.ObjectMapper;
 
 @RestController
 public class HelloController {
@@ -9,6 +12,13 @@ public class HelloController {
 	@GetMapping("/hello")
 	public Mensagem hello() {
 		return new Mensagem("Felipe Rassati Stephano", "Olá, mundo! Meu primeiro endpoint Spring Boot");
+	}
+
+	@GetMapping(value = "/hello-manual", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String helloManual() throws JacksonException {
+		Mensagem mensagem = new Mensagem("Felipe Rassati Stephano", "JSON gerado manualmente com ObjectMapper");
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(mensagem);
 	}
 }
 
