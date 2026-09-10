ccf4eaad5ea389ce2a8f3470f89989425fcd3a37
Rassati <feliperassati@gmail.com>
2026-09-10 12:53:56 -0300

    Feito ate a qeustão 2

---
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: c1b444ca998748a9ea402722c0bbb8b8039fd818f462591a31651fd968c93ad1
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: c1b444ca998748a9ea402722c0bbb8b8039fd818f462591a31651fd968c93ad1
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
